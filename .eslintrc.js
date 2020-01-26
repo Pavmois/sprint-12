6cb1b80366f49c4be3131958e80491da8cbbbc55
pavmois <pavmois@gmail.com>
2020-01-26 11:57:39 +0300

    version 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,6 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
         "airbnb-base"
     ],
     "globals": {
